b1d5c5f95eac572756cf8a9aedebc5adf9396d20
HenriqueRD <henriquethedordox@gmail.com>
2021-05-05 18:55:13 -0300

    Gráficos adicionados

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -6,11 +6,23 @@ import App from './App';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import DataTable from 'components/DataTable';
+import BarChart from 'components/BarChart';
+import DonutChart from 'components/DonutChart';
 
 ReactDOM.render(
   <React.StrictMode>
       <Header />
       <div className="container">
+        <h1 className="text-primary my-4">Painel de Controle</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+           <DonutChart />
+          </div>
+        </div>
+        <h1 className="text-primary my-4">Todas Vendas</h1>
         <DataTable />
       </div>
       <Footer />
