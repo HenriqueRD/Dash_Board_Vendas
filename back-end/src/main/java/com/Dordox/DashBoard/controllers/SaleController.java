56c18cd1d5b15be019e134c7087206c73fbe8269
HenriqueRD <henriquethedordox@gmail.com>
2021-05-11 17:51:29 -0300

    Paginação

--- back-end/src/main/java/com/Dordox/DashBoard/controllers/SaleController.java
+++ back-end/src/main/java/com/Dordox/DashBoard/controllers/SaleController.java
@@ -1,26 +1,26 @@
 package com.Dordox.DashBoard.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.Dordox.DashBoard.dto.SellerDTO;
-import com.Dordox.DashBoard.service.SellerService;
+import com.Dordox.DashBoard.dto.SaleDTO;
+import com.Dordox.DashBoard.service.SaleService;
 
 @RestController
-@RequestMapping(value = "/sellers")
-public class SellerController {
+@RequestMapping(value = "/sales")
+public class SaleController {
 	
 	@Autowired
-	private SellerService service;
+	private SaleService service;
 	
 	@GetMapping
-	public ResponseEntity<List<SellerDTO>> findAll() {
-		List<SellerDTO> list = service.findAll();
+	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
+		Page<SaleDTO> list = service.findAll(pageable);
 		
 		return ResponseEntity.ok(list);
 	}
---
+++ back-end/src/main/java/com/Dordox/DashBoard/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.Dordox.DashBoard.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Dordox.DashBoard.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
