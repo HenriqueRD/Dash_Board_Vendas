9bca0b13bba66712855aa7a7f821c4ab3bd1e76e
HenriqueRD <henriquethedordox@gmail.com>
2021-05-20 09:52:42 -0300

    Primeira requisição

--- front-end/src/components/DonutChart/index.tsx
+++ front-end/src/components/DonutChart/index.tsx
@@ -1,6 +1,26 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+
+type ChartData = {
+    labels: string[],
+    series: number[]
+}
 
 function DonutChart() {
+
+    let chartData : ChartData = { labels: [], series: [] };
+
+    axios.get(`${BASE_URL}/sales/sumBySeller`).then((response) => {
+        const data = response.data as SaleSum[];
+        const myLabels = data.map(x => x.sallerName);
+        const mySeries = data.map(x => x.sum);
+
+        chartData = { labels: myLabels, series: mySeries };
+        console.log(chartData);
+        
+    });
 
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
@@ -17,8 +37,8 @@ function DonutChart() {
         <div>
             <h5 className="text-center text-secondary">Participação nas Vendas (%)</h5>
                 <Chart
-                    options={{...options, labels: mockData.labels}}
-                    series={mockData.series}
+                    options={{...options, labels: chartData.labels}}
+                    series={chartData.series}
                     type="donut"
                     height="240"
                 />
